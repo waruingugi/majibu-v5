d58a475d01e337a1f07bd3bbdc87248255379f7b
waruingugi <waruingugientp@gmail.com>
2023-04-23 19:00:53 +0300

    Created sessions module

---
+++ app/sessions/routes/session.py
@@ -0,0 +1,16 @@
+from fastapi import Request, APIRouter
+from fastapi.responses import HTMLResponse
+
+from app.core.config import templates
+
+
+router = APIRouter()
+template_prefix = "session/templates"
+
+
+@router.get("/home/", response_class=HTMLResponse)
+async def get_home(request: Request):
+    """Get home page"""
+    return templates.TemplateResponse(
+        f"{template_prefix}home.html", {"request": request}
+    )
