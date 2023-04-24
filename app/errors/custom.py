05c210d32d8d69420156b4dd5059a81bdd87c770
waruingugi <waruingugientp@gmail.com>
2023-04-24 23:09:21 +0300

    Show errors in verification template

--- app/errors/custom.py
+++ app/errors/custom.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 class ErrorCodes(str, Enum):
     INVALID_PHONENUMBER = "The phone number {} is not valid"
-    INVALID_OTP = "The OTP entered is not correct. Please try again"
+    INVALID_OTP = "The code you entered is not correct. Please try again"
     OBJECT_NOT_FOUND = "The specified object does not exist"
     NO_CHANGES_DETECTED = "No changes were detected"
     INACTIVE_ACCOUNT = "This account is currently inactive. Please contact support"
