5709392456ff154630f40e3304ac872bb883420d
waruingugi <waruingugientp@gmail.com>
2023-05-01 18:04:52 +0300

    Created sessions model

--- app/db/base.py
+++ app/db/base.py
@@ -3,4 +3,5 @@
 from app.auth.models import *  # noqa
 from app.users.models import *  # noqa
 from app.notifications.models import *  # noqa
+from app.sessions.models import *  # noqa
 from app.db.base_class import Base  # noqa
