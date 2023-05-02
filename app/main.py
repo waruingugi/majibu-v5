274440b47eb11df70a56ad186400ddd53e5906de
waruingugi <waruingugientp@gmail.com>
2023-05-02 22:41:24 +0300

    Re-add exception segmentation

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
-from typing import List
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 
 from app.auth import api as auth_api
 from app.sessions import api as session_api
-from app.core.config import templates
 
-from starlette.exceptions import HTTPException as StarletteHTTPException
 from asgi_correlation_id import CorrelationIdMiddleware
 
 
@@ -14,6 +11,7 @@ from asgi_correlation_id import CorrelationIdMiddleware
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from app.core.ratelimiter import limiter
+from app.exceptions.handlers import register_exception_handlers
 
 
 app = FastAPI()
@@ -28,44 +26,4 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(auth_api.router)
 app.include_router(session_api.router)
 
-
-@app.exception_handler(StarletteHTTPException)
-async def http_exception_handler(request, exc):
-    server_errors: List = []
-
-    if hasattr(exc, "detail"):
-        server_errors.append(exc.detail)
-    if hasattr(exc, "error_message"):
-        server_errors.append(exc.error_message)
-
-    return templates.TemplateResponse(
-        "templates/info.html",
-        {"request": request, "server_errors": server_errors, "title": "New Message"},
-    )
-
-
-@app.exception_handler(429)
-async def custom_429_handler(request, __):
-    server_errors: List = ["Too many requests, please try again later"]
-    return templates.TemplateResponse(
-        "templates/info.html",
-        {"request": request, "server_errors": server_errors, "title": "New Message"},
-    )
-
-
-@app.exception_handler(404)
-async def custom_404_handler(request, __):
-    server_errors: List = ["The requested page could not be found"]
-    return templates.TemplateResponse(
-        "templates/info.html",
-        {"request": request, "server_errors": server_errors, "title": "New Message"},
-    )
-
-
-@app.exception_handler(401)
-async def custom_401_handler(request, __):
-    server_errors: List = []
-    return templates.TemplateResponse(
-        "auth/templates/login.html",
-        {"request": request, "server_errors": server_errors, "title": "Login"},
-    )
+register_exception_handlers(app)
