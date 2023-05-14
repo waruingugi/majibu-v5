b64d57f2091967cccd4940ce04fdb5c7b9b7411f
waruingugi <waruingugientp@gmail.com>
2023-05-14 15:04:59 +0300

    Add more tests for mpesa related routes

--- app/accounts/tests/routes/test_routes.py
+++ app/accounts/tests/routes/test_routes.py
@@ -25,7 +25,30 @@ def test_post_deposit_creates_model_instance(
     assert db_obj.merchant_request_id == "29115-34620561-1"
 
 
-def test_post_callback(db: Session, client: TestClient, mocker: MockerFixture):
+def test_post_callback_fails_with_http_exception(
+    db: Session, client: TestClient, mocker: MockerFixture
+):
+    data = mock_stk_push_response
+
+    mpesa_payment_dao.create(
+        db,
+        MpesaPaymentCreateSerializer(
+            phone_number=settings.SUPERUSER_PHONE,
+            merchant_request_id=data["MerchantRequestID"],
+            checkout_request_id=data["CheckoutRequestID"],
+            response_code=data["ResponseCode"],
+            response_description=data["ResponseDescription"],
+            customer_message=data["CustomerMessage"],
+        ),
+    )
+    response = client.post("/accounts/payments/callback/", json=mock_stk_push_result)
+
+    assert "Forbidden" in response.context["server_errors"]
+
+
+def test_post_callback_accepts_white_listed_ips(
+    db: Session, client: TestClient, mocker: MockerFixture
+):
     mock_client = mocker.patch("fastapi.Request.client")
     mock_client.host = MPESA_WHITE_LISTED_IPS[0]
 
@@ -42,4 +65,6 @@ def test_post_callback(db: Session, client: TestClient, mocker: MockerFixture):
             customer_message=data["CustomerMessage"],
         ),
     )
-    client.post("/accounts/payments/callback/", json=mock_stk_push_result)
+    response = client.post("/accounts/payments/callback/", json=mock_stk_push_result)
+
+    assert hasattr(response, "context") is False
