bc9685efb3bae79913a90b1d0c1ba24512b18e26
waruingugi <waruingugientp@gmail.com>
2023-09-02 13:37:33 +0300

    Remove line in heroku_test.sh

--- heroku_test.sh
+++ heroku_test.sh
@@ -3,8 +3,6 @@
 # THIS EXECUTABLE IS ONLY USED BY HEROKU
 # DO NOT RUN IT LOCALLY!!
 
-export $(grep -v '^#' .env.test | xargs)
-
 # Override these variables with those Heroku has provided
 echo "Overriding environment variables set by user..."
 export ASYNC_SQLALCHEMY_DATABASE_URI="$DATABASE_URL"
@@ -19,10 +17,3 @@ redis-server --daemonize yes
 
 echo "Clearing all redis data..."
 redis-cli FLUSHDB
-
-echo "Running tests $1"
-pytest --log-cli-level=WARNING -vv $1
-
-# Use this later
-# celery -A app.worker worker -Q scheduler-queue -l info
-# celery -A app.worker beat
