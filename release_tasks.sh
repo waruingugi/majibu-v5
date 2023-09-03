0975fb6809b032598c77a19b9251931c58fdc0f4
waruingugi <waruingugientp@gmail.com>
2023-09-03 10:23:19 +0300

    Add release tasks file

---
+++ release_tasks.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# This file is run by heroku befor launching dynos
+
+# Uncomment command below if you are having 
+# ´ModuleNotFoundError: No module named 'app´ errors 
+export PYTHONPATH=$PWD
+
+alembic upgrade head
+
+python app/backend_pre_start.py
